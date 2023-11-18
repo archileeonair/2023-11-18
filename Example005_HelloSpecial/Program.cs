473316ded20156926030d4bdf5e201b3f21d2d53
Artur Takhautdinov <arturtaha@gmail.com>
2023-11-18 22:38:26 +0300

    Добавили урок 5

---
+++ Example005_HelloSpecial/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "маша"){
+    Console.WriteLine("Ура! Это же Маша");
+}
+else{
+    Console.WriteLine("Привет, " + username + "!");
+}
