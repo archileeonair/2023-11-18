7270b95b86c1e6e0415ece4db8ce5cc10da92b51
Artur Takhautdinov <arturtaha@gmail.com>
2023-11-19 20:08:00 +0300

    Добавил выполнение заданий с урока 2 — введение в программирование

---
+++ Example010_MethodArray/Program.cs
@@ -0,0 +1,15 @@
+﻿int[] array = {1, 12, 31, 4, 15, 16, 17, 18};
+
+int n = array.Length;
+int find = 4;
+
+int index = 0;
+
+while(index<n){
+    if(array[index] == find){
+        Console.WriteLine("Искомое значение: " + array[index]);
+        Console.WriteLine("Искомый индекс: " + index);
+        break;
+    }
+    index++;
+}
